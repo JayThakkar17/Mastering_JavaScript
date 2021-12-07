869f1fcf4b073d1c28cda9d65a5663c45cfc0654
Jay Thakkar <jayhthakkar99@gmail.com>
2021-12-07 22:07:57 +0530

    Function Added

---
+++ functions/functions.js
@@ -0,0 +1,18 @@
+function logger() {
+  //function body starts
+  console.log('Hello!, My Name is Jay.');
+  //function body ends
+}
+
+const ES6Logger = () => {
+  //function body starts
+  console.log('Hello From ES6 Function!');
+  //function body ends
+};
+
+//Invoking / Running / Calling function
+logger();
+logger();
+
+ES6Logger();
+ES6Logger();
